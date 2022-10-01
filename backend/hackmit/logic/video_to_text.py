a4be7093f1521b784efca77069eb378396addd36
Joy Liu <joyliu.q@gmail.com>
2022-10-01 16:44:46 -0400

    :art: Better code

--- backend/hackmit/logic/video_to_text.py
+++ backend/hackmit/logic/video_to_text.py
@@ -13,22 +13,21 @@ class TranscriptSegment:
   def __repr__(self):
     return str(self)
 
-model = whisper.load_model("base")
-options = whisper.DecodingOptions(language="en")
+class VideoToText:
+  model = whisper.load_model("base")
+  options = whisper.DecodingOptions(language="en")
 
-def mp4_to_mp3(mp4_path, mp3_path):
-  os.system(f"ffmpeg -i {mp4_path} {mp3_path}")
+  def __init__(self, video_path):
+      audio_path = video_path + "_audio.mp3"
+      mp4_to_mp3(video_path, audio_path)
+      transcribe_audio(video_path)
 
-def transcribe_audio(audio_path):
-  print(whisper.load_audio(audio_path))
-  transcript = model.transcribe(audio_path)
-  transcript['segments'] = [TranscriptSegment(s['text'], s['start'], s['end']) for s in transcript['segments']]
-  print(transcript)
-  return transcript
-  
-def video_to_text(video_path):
-  audio_path = video_path + "_audio.mp3"
-  mp4_to_mp3(video_path, audio_path)
-  transcribe_audio(video_path)
+  def mp4_to_mp3(mp4_path, mp3_path):
+    os.system(f"ffmpeg -i {mp4_path} {mp3_path}")
 
-video_to_text("../../data/barackobamafederalplaza.mp3")
+  def transcribe_audio(audio_path):
+    print(whisper.load_audio(audio_path))
+    transcript = model.transcribe(audio_path)
+    transcript['segments'] = [TranscriptSegment(s['text'], s['start'], s['end']) for s in transcript['segments']]
+    print(transcript)
+    return transcript    
