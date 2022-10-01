888e1be3d22312c5e6ee45bcb750f6acd9265abb
Joy Liu <joyliu.q@gmail.com>
2022-10-01 19:42:37 -0400

    :art: Migration to Fast API

---
+++ backend/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+@app.get("/hello")
+def hello_view(name: str = "Human"):
+    return {"message": f"Hello there, {name}!"}
+
+@app.get("/add-music")
+def add_music(file: bytes = File(...)):
+    if len(file) == 0:
+        raise HTTPException(status_code=400, detail="File must have some content.")
+    if len(file) > 10 << 20:
+        raise HTTPException(status_code=400, detail="File must be less than 10MB.")
+    
+    # TODO: call video_to_text
+
+    # TODO: call kincent/daniel's code to figure out sentiments
+
+    # TODO: from sentiment, figure out which musics to insert
+
+    # TODO: add music
+
+    # TODO: return the actual clip as an output? or S3 bucket lol idk
+
+    return {"music": "TODO"}
