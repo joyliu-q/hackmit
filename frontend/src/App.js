4b506c7b87669d384be4b525d332e19054dde749
Jinay Jain <jinaybjain@gmail.com>
2022-10-02 09:54:32 -0400

    Centered video preview page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,11 @@
 import React, { useState } from "react";
-import "./good_video.mp4";
-import Plyr from "plyr-react";
-import "plyr-react/plyr.css";
-import Dropzone from "dropzone";
+import "./App.css";
+import Video from "./pages/Video";
 
 const axios = require("axios");
 
 function App() {
   const [video, setVideo] = useState(null);
-  Dropzone.autoDiscover = false;
-
-  React.useEffect(() => {
-    let myDropzone = new Dropzone("div.my-dropzone", { url: "/file/post" });
-    myDropzone.on("addedfile", async (video) => {
-      console.log(`File added: ${video.name}`);
-      setVideo(video);
-      // TODO: await resp
-    });
-  }, []);
 
   async function handleSubmit(event) {
     const video = event.target.video.files[0];
@@ -33,26 +21,7 @@ function App() {
     event.preventDefault();
   }
 
-  return (
-    <div className="App">
-      {video && (
-        <>
-          <video width="750" height="500" controls>
-            <source
-              src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-              type="video/mp4"
-            />
-          </video>
-        </>
-      )}
-      <header className="App-header">
-        <form onSubmit={handleSubmit}>
-          <input type="file" id="video" name="video" accept="video/*" />
-          <input type="submit" value="Submit" />
-        </form>
-      </header>
-    </div>
-  );
+  return <Video />;
 }
 
 export default App;
