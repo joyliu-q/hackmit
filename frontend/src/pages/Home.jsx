dbcefefc21f44d9ae4951d3e8c8e257f9d653e55
Joy Liu <joyliu.q@gmail.com>
2022-10-02 10:15:12 -0400

    :art: Fix home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import { Box, Typography, Button } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import Plyr from "plyr-react";
+import "plyr-react/plyr.css";
+import React, { useState } from 'react';
 
+const axios = require("axios");
 
 const StyledButton = styled(Button)({
     '&:hover': {
@@ -45,7 +49,25 @@ const ContainerBox = styled(Box) ({
 
 const Home = () => {
 
+    const [videoSrc, setVideoSrc] = useState(null);
 
+    async function handleSubmit(event) {
+      event.preventDefault();
+      const video = event.target.video.files[0];
+      console.log(video);
+      setVideoSrc(video);
+  
+      const response = await axios.post(
+        "http://localhost:8000/add-music",
+        { file: video },
+        { headers: { "Content-Type": "multipart/form-data" } }
+      );
+      console.log(response);
+      console.log(response.data.path);
+      console.log(videoSrc)
+      setVideoSrc(response.data.path);
+    }
+    
     return (
         <ContainerBox>
             <Box sx={{ height: '30vh', padding: '40px', display: 'flex' }}>
@@ -54,21 +76,41 @@ const Home = () => {
                     <Typography variant="h4" sx={{color: '#939393'}}>Automatically generated background music and high-quality captions for any video</Typography>
                 </Box>
                 <StyledBox sx={{flex: 2, marginLeft: '200px'}}>
-                    <CenteredBox>
-                         <form
+                    {videoSrc ? <Plyr  {
+                        ...{
+                            source: {
+                                type: 'video',
+                                title: 'Example title',
+                                sources: [
+                                    {
+                                        src: videoSrc,
+                                        type: 'video/mp4',
+                                        size: 720,
+                                    }
+                                ]
+                            }
+                        }}
+                    /> :
+                        <CenteredBox>
+                            {/* <form
                             action="http://0.0.0.0:8000/add-music"
                             method="POST"
                             encType="multipart/form-data"
                         >
                             <CenteredButton type="file" name="file" component="label" variant="contained">Upload<input type="file" hidden onChange={'form.submit()'}></input></CenteredButton>
-                        </form>
+                        </form> */}
+                            <form onSubmit={handleSubmit}>
+                                {/* <input type="file" id="video" name="video" accept="video/*" />
+                            <input type="submit" value="Submit" /> */}
+                                <CenteredButton type="file" name="file" component="label" variant="contained">Upload<input type="file" hidden onChange={'form.submit()'}></input></CenteredButton>
+                            </form>
 
-                        <Typography sx={{top: '40%', textAlign: 'center'}}variant="h4">Drop files or upload files here</Typography>
+                            <Typography sx={{ top: '40%', textAlign: 'center' }} variant="h4">Drop files or upload files here</Typography>
                         
-                    </CenteredBox>
+                        </CenteredBox>
+                    }
                 </StyledBox>
             </Box>
-
         </ContainerBox>
     )
 }
